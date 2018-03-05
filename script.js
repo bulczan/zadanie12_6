15d68d0af55c515cc611e22c58ea6412e172a86e
wypior <w.wypior@f5.com>
2018-03-05 15:49:16 +0000

    main code works

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
 var url = 'https://restcountries.eu/rest/v2/name/';
 $('#search').click(searchCountries);
-var searches = [];
+var buffer = [];
 
 function searchCountries() {
     var countryName = $('#country-name').val();
-if (!countryName.length) countryName = 'Poland';
-$.ajax({
-        url: url + countryName,
-        method: 'GET',
-        success: buildOutput
-    });
+    if (!countryName.length) countryName = 'Poland';
+    if (!checkQuery(countryName)) {
+        $.ajax({
+            url: url + countryName,
+            method: 'GET',
+            success: buildOutput
+        });
+    }
+}
+
+function checkQuery(selection) {
+    var result = false;
+    if(buffer.length > 0){
+        for(var i = 0; i < buffer.length; i++){
+            buffer[i].forEach(function(item){
+                if(item.name === selection) {
+                    result = true;
+                }
+            })
+
+        }
+    }
+    return result
+}
+
+function randomString() {
+    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+    var str = '';
+    for (var i = 0; i < 10; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+}
+
+function buildOutput(resp) {
+    if (buffer.length === 4) {
+        buffer.shift();
+        $('.main').children().last().remove();
+    }
+    if (buffer.length === 0) {
+        $('.main').append(buildContainer(randomString()));
+    }
+    else {
+        $('.main').prepend(buildContainer(randomString()));
+    }
+    showCountriesList(resp);
+    buffer.push(resp);
 }
 
 function buildTable() {
@@ -34,22 +75,14 @@ function buildRow(header) {
     return $row
 }
 
-function buildContainer(integer) {
-    var $container = $('<div></div>').attr('id', integer.toString());
+function buildContainer(id) {
+    var $container = $('<div></div>').attr('id', id);
     var $h2 = $('<h2></h2>').attr('id', 'country');
-    var $info = $('<div></div>').addClass('back').text('Background Information:');
+    var $info = $('<div></div>').attr('id', 'info').text('Background Information:');
     $container.append($h2)
               .append($info)
-              .append(buildTable().insertAfter(".back"));
+              .append(buildTable());
     return $container
-}
-
-function buildOutput(resp) {
-    var result = buildContainer(searches.length);
-
-    showCountriesList(resp);
-    searches.push(result);
-
 }
 
 function showCountriesList(resp) {
